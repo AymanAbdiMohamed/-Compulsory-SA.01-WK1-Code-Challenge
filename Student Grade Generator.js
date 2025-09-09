a6f9a5375b5a8478f2a3ca74c390aa6b8840540e
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-09-09 23:48:01 +0300

    done with Student Grade Generator.js

---
+++ Student Grade Generator.js
@@ -0,0 +1,24 @@
+
+
+function generateGrade(marks) {
+    if (marks > 100 || marks < 0) {
+        return "Invalid input. Marks should be between 0 and 100.";
+    }
+
+    if(marks > 79) {
+        return "A"
+    }
+    else if (marks >= 60) {
+        return "B"
+    }
+    else if (marks >= 50) {
+        return "C"
+    }
+    else if (marks >= 40) {
+        return "D"
+    }
+    else {
+        return "E"
+    }
+}
+console.log(generateGrade(85))
