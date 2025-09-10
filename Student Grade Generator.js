9849c8750b041540182d1e73469c53d46d2e4426
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-09-10 07:51:26 +0300

    added comments to the studentgradegenerator codebase

--- Student Grade Generator.js
+++ Student Grade Generator.js
@@ -1,24 +1,24 @@
 
-
+//create function to grade the marks, with limits of 0 to 100. Above or below the range results in invalid input 
 function generateGrade(marks) {
     if (marks > 100 || marks < 0) {
         return "Invalid input. Marks should be between 0 and 100.";
     }
-
     if(marks > 79) {
-        return "A"
+        return "A" //marks greater than 79
     }
     else if (marks >= 60) {
-        return "B"
+        return "B" //marks greater than 60 less than 79
     }
     else if (marks >= 50) {
-        return "C"
+        return "C" //marks greater than 50 less than 60
     }
     else if (marks >= 40) {
-        return "D"
+        return "D" //marks greater than 40 less than 50
     }
     else {
-        return "E"
+        return "E" //marks less than 40
     }
 }
+//e.g run
 console.log(generateGrade(85))
