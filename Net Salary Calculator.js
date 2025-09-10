82cef4e6ff5d2295a7c482784f761977cd57f57a
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-09-10 08:02:51 +0300

    added comments to the netsalarycalculator codebase

--- Net Salary Calculator.js
+++ Net Salary Calculator.js
@@ -2,14 +2,20 @@
 
 
 function calculateNetSalary(basicSalary, benefits) {
+    //calculate gross salary
+
     let grossSalary = basicSalary + benefits;
+
+    //deduction of tax (10%), nhif and nssf(6%) from grossSalary
     let paye = 0.1 * grossSalary;
     let nhif = 1500;
     let nssf = 0.06 * grossSalary;
+    //Add up all the deductions
     let totalDeductions = paye+ nhif + nssf;
+    //Subtract it from the grossSalary
     let netSalary = grossSalary - totalDeductions;
 
-
+    //print out everything before running the file
     console.log("Gross Salary", grossSalary);
     console.log("PAYE (Taxation):", paye);
     console.log("NHIF:", nhif);
